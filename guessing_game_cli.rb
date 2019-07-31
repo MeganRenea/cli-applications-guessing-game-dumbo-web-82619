9bb706d7356058127e33bc1db5484a43cb9f2a6a
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-31 01:49:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,8 @@
 # Code your solution here!
+def run_guessing_game
+  random_number = rand(1..6)
+  puts "Guess a number between 1 and 6"
+end
 - Generate and store a random number between 1 and 6
 - Prompts the user to guess their own number between 1 and 6
 - Capture user input from the command line
